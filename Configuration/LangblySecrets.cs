4ded47e327659bbd22a07d195397bd9ce7c190c0
NgoTruongVu05 <ngotruongvu05@gmail.com>
2026-04-18 19:50:01 +0700

    Delete Configuration/LangblySecrets.cs

--- Configuration/LangblySecrets.cs
+++
@@ -1,8 +0,0 @@
-namespace FoodStreetMAUI.Configuration
-{
-    public static class LangblySecrets
-    {
-        public const string BaseUrl = "https://api.langbly.com/language/translate/v2";
-        public const string ApiKey = "CxENGTZZFjFuHcvVXiugTd";
-    }
-}
